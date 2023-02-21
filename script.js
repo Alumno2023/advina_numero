0106e42c5f49aab2f948215afad870ffd966ba51
alumno <sallndearame@gmail.com>
2023-02-21 11:11:41 +0100

    Otros más

---
+++ script.js
@@ -0,0 +1,8 @@
+const primerParrafo = document.querySelector('p')
+console.log(primerParrafo)
+const parrafoImportante = document.querySelector('#importante')
+primerParrafo.style.color = 'red'
+parrafoImportante.style.console = 'blue'
+parrafoImportante.style.fontSize = '3rem'
+parrafoImportante.style.backgroundColor = 'yellow' /*PARA EL BACKGROUND COLOR*/
+parrafoImportante.textContent = 'ESTE ES EL PARRAFO SUPER IMPORTANTE'
---
+++ script1.js
@@ -0,0 +1,2 @@
+const scorefield = document.querySelector('score') // 1 paso para selecciona el elemenro DOM y guardarlos
+scorefield.textContent = 10
